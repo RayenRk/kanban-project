012e043fd1a6bbdbb8b96045f3418bffa7adf022
SouFien19 <soufienlabiadh19@gmail.com>
2024-05-09 12:01:35 +0100

    add front for all items

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DragDropComponent, RegisterComponent,LoginComponent],
+  imports: [RouterOutlet, DragDropComponent, RegisterComponent,LoginComponent,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,12 +2,20 @@ import { Routes } from '@angular/router';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect root path to drag-drop
-  { path: 'drag-drop', component: DragDropComponent }, // Drag & Drop route
+  { path: 'board', component: DragDropComponent }, // Drag & Drop route
   { path: 'login', component: LoginComponent }, // Login route
   { path: 'register', component: RegisterComponent }, // Register route
+  { path: 'new-user', component: UsersComponent },
+  { path: 'new-project', component: ProjectsComponent },
+  { path: 'new-task', component: TasksComponent },
 ];
+
 
 export { routes }; 
